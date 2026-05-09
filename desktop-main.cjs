8b7d4d1e78f3d9f97c310c019d65a2df1446bda0
Hendrik Kaiser <hendrik@kaiser-mail.de>
2026-05-09 21:46:48 +0200

    Add desktop Electron launcher

---
+++ desktop-main.cjs
@@ -0,0 +1,32 @@
+const { app, BrowserWindow, Menu } = require('electron');
+const path = require('path');
+
+function createWindow() {
+  const win = new BrowserWindow({
+    width: 1440,
+    height: 960,
+    minWidth: 1100,
+    minHeight: 760,
+    title: 'BAR Map Generator',
+    backgroundColor: '#050505',
+    autoHideMenuBar: true,
+    webPreferences: {
+      contextIsolation: true,
+      nodeIntegration: false,
+      sandbox: true,
+    },
+  });
+
+  Menu.setApplicationMenu(null);
+  win.loadFile(path.join(__dirname, 'bar_map_generator.html'));
+}
+
+app.whenReady().then(createWindow);
+
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') app.quit();
+});
+
+app.on('activate', () => {
+  if (BrowserWindow.getAllWindows().length === 0) createWindow();
+});
